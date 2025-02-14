499401e0ba7d220fedd04a146699a08b9771b156
Stephen Mayer <66144707+Stephen-Mayer@users.noreply.github.com>
2025-02-13 22:01:04 -0500

    Updated main.h

--- main.h
+++ main.h
@@ -20,13 +20,4 @@
 
 using namespace std;
 
-/*
-Instructions to run:
-Open terminal and access Final Project folder
-Do the following commands:
-make
-./20Questions input_layer_weights.txt input_biases.txt hidden_layer_weights.txt hidden_biases.txt classes.txt predicates.txt predicate-matrix-binary.txt twentyquestions.txt
-./20Questions randomized_input_layer_weights.txt randomized_input_biases.txt randomized_hidden_layer_weights.txt randomized_hidden_biases.txt classes.txt predicates.txt predicate-matrix-binary.txt twentyquestions.txt
-
-*/
 #endif
